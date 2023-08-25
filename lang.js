dae0ca793c752a810ce315802c76c3164843a7c8
Victoria Domovets <v.domovets@gmail.com>
2023-08-25 16:34:59 +0200

    updates

--- lang.js
+++ lang.js
@@ -1,3 +1,70 @@
 const langArr = {
-  
+  "name": {
+    "en": "Victoria Domovets",
+    "ru": "Виктория Домовец",
+  },
+  "prof": {
+    "en": "front-end developer",
+    "ru": "фронтенд-разработчик",
+  },
+  "skills": {
+    "en": "Hard skills",
+    "ru": "Профессиональные навыки",
+  },
+  "layout": {
+    "en": "responsys development",
+    "ru": "адаптивная и кроссбраузерная верстка",
+  },
+  "test": {
+    "en": "manual testing",
+    "ru": "ручное тестирование",
+  },
+  "education": {
+    "en": "Education",
+    "ru": "Профильное образование",
+  },
+  "education-value": {
+    "en": "HTML Academy Saint Petersburg: front-end developer online course. 2020-2022",
+    "ru": "HTML Academy г.Санкт-Петербург: онлайн курс по профессии 'Фронтенд-разработчик'. 2020-2022гг.",
+  },
+  "education-link": {
+    "en": "Certificates",
+    "ru": "Ссылка на профиль в академии",
+  },
+  "projects": {
+    "en": "Projects",
+    "ru": "Проекты",
+  },
+  "soft": {
+    "en": "Soft skills",
+    "ru": "Личные качества",
+  },
+  "soft1": {
+    "en": "Soft skills",
+    "ru": "Личные качества",
+  },
+  "soft2": {
+    "en": "responsibilty",
+    "ru": "ответственность",
+  },
+  "soft3": {
+    "en": "attention to details",
+    "ru": "внимательность",
+  },
+  "soft4": {
+    "en": "problem-solving skill",
+    "ru": "самостоятельность",
+  },
+  "soft": {
+    "en": "critical thinking",
+    "ru": "критичность ума",
+  },
+  "soft5": {
+    "en": "applicability",
+    "ru": "обучаемость",
+  },
+  "lang": {
+    "en": "Spoken languages",
+    "ru": "Языки",
+  },
 }
